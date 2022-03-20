6aacb78a6a63044d9be39ef652fed70074ba81c7
poocong <akuuntumball01@gmail.com>
2022-03-20 17:36:45 +0000

    PocongUserbot: auto fixes
    Signed-off-by: poocong <akuuntumball01@gmail.com>

--- userbot/modules/other.py
+++ userbot/modules/other.py
@@ -217,12 +217,12 @@ async def _(event):
     web = BYPASS_URL
     async with event.client.conversation(web) as conv:
         try:
-            msg = await conv.send_message(d_link)
+            await conv.send_message(d_link)
             details = await conv.get_response()
             await event.client.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
             await event.client(UnblockRequest(web))
-            msg = await conv.send_message(d_link)
+            await conv.send_message(d_link)
             details = await conv.get_response()
             await event.client.send_read_acknowledge(conv.chat_id)
         await event.client.send_message(event.chat_id, details.message)
