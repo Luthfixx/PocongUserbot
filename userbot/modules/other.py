e62e99b77a703a0c77cccff419d18bfff650775b
Luthfixx <101885847+Luthfixx@users.noreply.github.com>
2022-03-21 05:10:48 +0800

    Update other.py

--- userbot/modules/other.py
+++ userbot/modules/other.py
@@ -203,19 +203,7 @@ async def _(event):
 
 @poci_cmd(pattern="bypass(?: |$)(.*)")
 async def _(event):
-    xx = await edit_or_reply(fry, "`Melakukan bypass..`")
-    url = event.pattern_match.group(2)
-    if event.fwd_from:
-        return
-    if not event.reply_to_msg_id:
-        await edit_delete(xx, "**Silahkan Balas Ke Media Foto**")
-        return
-    reply_message = await event.get_reply_message()
-    else:
-        return await edit_delete(
-            event,
-            "**Mohon berikan link valid..**",
-        )
+    level = fry.pattern_match.group(2)
     return
     xx = await edit_or_reply(event, "`Melakukan direct link...`")
     web = BYPASS_URL
@@ -226,7 +214,7 @@ async def _(event):
             await event.client.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
             await event.client(UnblockRequest(web))
-            await conv.send_message(d_link)
+            await conv.send_message("/bypass {url}")
             details = await conv.get_response()
             await event.client.send_read_acknowledge(conv.chat_id)
         await event.client.send_message(event.chat_id, details.message)
