c41d5de65e7ba94450cb6816bba8f49cd27dc884
poocong <akuuntumball01@gmail.com>
2022-03-20 21:11:09 +0000

    PocongUserbot: auto fixes
    Signed-off-by: poocong <akuuntumball01@gmail.com>

--- userbot/modules/other.py
+++ userbot/modules/other.py
@@ -203,7 +203,7 @@ async def _(event):
 
 @poci_cmd(pattern="bypass(?: |$)(.*)")
 async def _(event):
-    level = fry.pattern_match.group(2)
+    fry.pattern_match.group(2)
     return
     xx = await edit_or_reply(event, "`Melakukan direct link...`")
     web = BYPASS_URL
